440f6b4cce9277e0cca074f98e250a76544c17aa
Marco-Antonio-Fernandes <marcoa.fer.dev@gmail.com>
2026-03-25 09:40:59 -0300

    Organizando estrutura: Java e Laravel

---
+++ back/src/main/java/br/unipar/rotatix/RotatixApplication.java
@@ -0,0 +1,13 @@
+package br.unipar.rotatix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotatixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotatixApplication.class, args);
+	}
+
+}
